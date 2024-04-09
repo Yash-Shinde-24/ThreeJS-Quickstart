37b8a85dd185e500a45c9bada9f3428e77aa344b
Yash Shinde <yashreya999@gmail.com>
2024-04-09 19:10:40 +0530

    Added scene and vision pro model in models folder

---
+++ main.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 1;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xeeeeee); // Set light gray background color
+document.body.appendChild(renderer.domElement);
+
+// Lighting
+
+// Bonus
+
+
+// OrbitControls
+
+// Load a GLTF model
+
+// Animation Loop
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
