9b1792c83b8f3bf0124255e19c7738a1b3a19cd3
Yash Shinde <yashreya999@gmail.com>
2024-04-09 19:12:46 +0530

    Added ambient and directional Lights

--- main.js
+++ main.js
@@ -16,7 +16,11 @@ renderer.setClearColor(0xeeeeee); // Set light gray background color
 document.body.appendChild(renderer.domElement);
 
 // Lighting
-
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(0, 1, 0);
+scene.add(directionalLight);
 // Bonus
 
 
